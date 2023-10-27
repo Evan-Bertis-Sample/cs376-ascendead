3f918ad69214d25e3ebd0ab92c16cb43a647549c
Shrimp-Chip <esample21@gmail.com>
2023-10-26 22:36:01 -0500

    Camera system

--- Assets/Code/Gameplay/CameraController/CameraController.cs
+++ Assets/Code/Gameplay/CameraController/CameraController.cs
@@ -14,39 +14,46 @@ namespace Ascendead.Components
         [field: SerializeField] public Transform Target { get; private set; }
 
         [field: Header("Camera Peeking Settings")]
-        [field: SerializeField] public float CameraPeekDistance { get; private set; } = 1f;
-        [field: SerializeField] public float CameraPeekSpeed { get; private set; } = 1f;
-        [field: SerializeField] public AnimationCurve CameraPeekCurve { get; private set; } = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
+        [field: SerializeField] public float CameraPeekDistance { get; private set; } = 5f;
+        [field: SerializeField] public float CameraLerpSpeed { get; private set; } = 10f;
         [field: SerializeField, InputPath] public string CameraPeekInput { get; private set; }
 
         [GlobalDefault] private InputManager _inputManager;
         [SerializeField] private Vector2Int _gridIndex;
+        private float _cameraZ;
 
         private void Start()
         {
             if (Target == null) throw new System.Exception("CameraController requires a target to function.");
             DependencyInjector.InjectDependencies(this);
+            _cameraZ = transform.position.z;
         }
 
         private void Update()
         {
             if (Target == null) return;
             _gridIndex = GetGridIndex(Target.position);
-            Vector2 position = GetGridPosition(_gridIndex);
+            Vector3 position = GetGridPosition(_gridIndex);
 
-            transform.position = position;
+            Vector2 peekInput = _inputManager.ReadInput<Vector2>(CameraPeekInput);
+            int peakDirection = Mathf.RoundToInt(peekInput.y);
+
+            position.y += CameraPeekDistance * peakDirection;
+            
+            transform.position = Vector3.Lerp(transform.position, position, Time.deltaTime * CameraLerpSpeed);
         }
 
         private Vector2Int GetGridIndex(Vector2 position)
         {
-            Vector2 offset = position - GridOrigin;
+            Vector2 offset = position - GridOrigin + new Vector2(GridSize.x / 2f, GridSize.y / 2f);
             Vector2Int index = new Vector2Int(Mathf.FloorToInt(offset.x / GridSize.x), Mathf.FloorToInt(offset.y / GridSize.y));
             return index;
         }
         
-        private Vector2 GetGridPosition(Vector2Int index)
+        private Vector3 GetGridPosition(Vector2Int index)
         {
-            Vector2 position = GridOrigin + new Vector2(index.x * GridSize.x, index.y * GridSize.y);
+            Vector3 position = GridOrigin + new Vector2(index.x * GridSize.x, index.y * GridSize.y);
+            position.z = _cameraZ;
             return position;
         }
 
